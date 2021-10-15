78be70dd49e4aa4e9e0887dfcad92f92a765607b
DinhVu <dinhvu@DinhVus-MacBook-Pro.local>
2021-10-14 23:17:20 -0700

    do part3_v1

---
+++ timing.py
@@ -0,0 +1,16 @@
+import time
+def calculate_time(function):
+	def wrapper():
+		start = time.time()
+		function()
+		end = time.time()
+		total = end - start
+		print(total)
+	return wrapper
+
+def sayHi():
+	time.sleep(2)
+
+sayHi_v2 = calculate_time(sayHi)
+
+sayHi_v2()
