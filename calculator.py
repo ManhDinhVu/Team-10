11835726c167c5dc0f2981f549233cff5d3585ca
DinhVu <dinhvu@DinhVus-MacBook-Pro.local>
2021-10-14 23:12:22 -0700

    do part2_v1

---
+++ calculator.py
@@ -0,0 +1,39 @@
+def calculator(number1, number2, operator):
+	result = 0
+	if type(number1) == int or float and type(number2) == int or float and operator in ('+','-','*','/','//','**'):
+		try:
+			num1 = float(number1)
+			num2 = float(number2)
+			if operator == '+':
+				result= num1 + num2
+			elif operator == '-':
+				result= num1 - num2
+			elif operator == '*':
+				result= num1 * num2
+			elif operator == '/':
+				result= num1 / num2
+			elif operator == '//':
+				result= num1 // num2	
+			elif operator == '**':
+				result= num1 ** num2
+		except ZeroDivisionError:
+			print("Can not divide by Zero")
+			return False
+		return result
+	else:
+		print('Invalid value/operator!!!')
+		return False
+
+def parse_input():
+	prompt = eval("input('Enter equation: ')")
+	text = prompt.split(' ')
+	if len(text) == 3:
+		num1 = text[0]
+		num2 = text[2]
+		operator = text[1]
+	else:
+		print('Not valid!')
+		exit()
+	return calculator(num1,num2,operator)
+
+print(parse_input())
